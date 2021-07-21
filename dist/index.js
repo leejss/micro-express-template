bb30eae6babb47c9d53eb664789a316fb1d88f54
leejss <goldemshine+3@gmail.com>
2021-07-21 17:52:48 +0900

    forgot

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-const Application_1 = require("./config/Application");
-exports.default = new Application_1.Application();
-//# sourceMappingURL=index.js.map
